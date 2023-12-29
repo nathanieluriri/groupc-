100c16cfc66e2954b8b0aa011623bd7303c888e9
nathanieluriri <uririnathaniel@gmail.com>
2023-12-29 20:59:19 +0100

    added messages to the run once fuction

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .viewerBadge_link__qRIco{display:None;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)

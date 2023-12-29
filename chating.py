95366fcb9b8ba5fd5a50ced29cdf0edbbb30d3a2
nathanieluriri <uririnathaniel@gmail.com>
2023-12-29 21:33:56 +0100

    update

--- chating.py
+++ chating.py
@@ -4,6 +4,8 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 def display_previous_chats ():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -12,10 +14,14 @@ def display_previous_chats ():
 
 
 def user(Prompt):
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     user_prompt = st.chat_message("user")
     user_prompt.write(Prompt)
     st.session_state.messages.append({"role":"user","content":Prompt})
 def bot_response(Prompt):
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     user_prompt = st.chat_message("assistant")
     user_prompt.write(Prompt)
     st.session_state.messages.append({"role":"assistant","content":Prompt})
